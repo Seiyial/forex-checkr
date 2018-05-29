d7837e5a23f007d4be11edd1a8a313213bbb38c6
Seiyial <seiyial@gmail.com>
2018-05-29 21:19:13 +0800

    Fixer API Endpoint (handling etc is on its way)

--- src/server/helpers/handleFixerAPIResponse.js
+++ src/server/helpers/handleFixerAPIResponse.js
@@ -1,16 +1,19 @@
 exports.default = function (error, response, body, searchVal1, searchVal2) {
-  console.log('error', error, 'response', response, 'body', body)
+  console.log('body', body)
 
   if (response.status !== 200) {
+
     return({
       status: 'fail',
       message: 'Service Error! Please contact tech support (Fixer API did not return 200)'
     })
 
+  // Requests receives data from API --
   } else if (body.success) {
     const rates = body.rates
     const hasRates = rates && rates[searchVal1] && rates[searchVal2]
-    return(hasRates ? 
+    return(hasRates ?
+      // --> has match for searched values
       {
         status: 'success',
         searchVal1,
@@ -18,6 +21,7 @@ exports.default = function (error, response, body, searchVal1, searchVal2) {
         displayRate: rates[searchVal1]
       }
     :
+      // --> no match for searched Values
       {
         status: 'fail',
         message: 'Service Error! Please contact tech support (Fixer API returned success without rate object)',
@@ -26,12 +30,14 @@ exports.default = function (error, response, body, searchVal1, searchVal2) {
 
   } else if (body.error && body.error.code === 202) {
     return({
+      // --> Fixer-side message for no match of searched values
       status: 'fail',
       message: `You requested ${searchBox1} and ${searchBox2}. Please check that these are valid currencies, and try again.`
     })
 
   } else if (body.error) {
     return({
+      // --> Miscellaneous Fixer-side error
       status: 'fail',
       message: `Service Error! Please contact tech support
       (Fixer API Error ${body.error.code}: ${body.error.message})`
@@ -39,6 +45,7 @@ exports.default = function (error, response, body, searchVal1, searchVal2) {
 
   } else {
     return({
+      // --> No error object returned from Fixer
       status: 'fail',
       message: `Service Error! Please contact tech support
       (Fixer API Error (null error object))`
--- src/server/index.js
+++ src/server/index.js
@@ -1,17 +1,23 @@
-require('dotenv').config()
+const dotEnvLoad = require('dotenv').config()
 const express = require('express')
 const request = require('request')
 const app = express()
-const formatFixerAPIResponse = require('./helpers/formatFixerAPIResponse')
+const handleFixerAPIResponse = require('./helpers/handleFixerAPIResponse').default
+
+console.log('(*) dotEnvLoad =>', dotEnvLoad)
 
 // ----------------------------------------------------
-
+// Displayed Routes
 app.use(express.static('dist')) // webpack bundle folder
 
-app.get('/fixer_api', function (req, res) {
-  request(
-    `http://data.fixer.io/api/latest?access_key=${process.env.FIXER_API_KEY}`, 
-    formatFixerAPIResponse
+// ----------------------------------------------------
+// API Routes
+app.get('/fixer_api/:searchVal1/:searchVal2', function (req, res) {
+  const { searchVal1, searchVal2 } = req.params
+  console.log('(*) process.env.FIXER_API_ACCESS_KEY =>', process.env.FIXER_API_ACCESS_KEY)
+  const result = request(
+    `http://data.fixer.io/api/latest?access_key=${process.env.FIXER_API_ACCESS_KEY}`,
+    (error, response, body) => res.json(handleFixerAPIResponse(error, response, body, searchVal1, searchVal2))
   )
 })
 
